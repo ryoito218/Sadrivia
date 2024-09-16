cf6bf19f2fc8560b3f6fafa3d3ca8b383eba4b0b
ryoito218 <ryoito218@gmail.com>
2024-09-16 14:09:55 +0900

    Add detail view

--- frontend/src/app/detail/[id]/[language]/page.tsx
+++ frontend/src/app/detail/[id]/[language]/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const DetailView = async ({params}: {params: {id: string; language: string }}) => {
@@ -6,19 +7,68 @@ const DetailView = async ({params}: {params: {id: string; language: string }}) =
   const trivia = await res.json();
 
   return (
-    <div className="">
-      <h1>
-        {
-          params.language === "japanese" ? trivia.japanese :
-          params.language === "english" ? trivia.english:
-          params.language === "chinese1" ? trivia.chinese1:
-          params.language === "chinese2" ? trivia.chinese2:
-          params.language === "korean" ? trivia.korean:
-          params.language === "mongolian" ? trivia.mongolian:
-          trivia.japanese
-        }
-      </h1>
-      <p></p>
+    <div>
+      <div className="container mx-auto">
+        <div className="flex flex-col m-3">
+          <div className="flex justify-center">
+            <div className="flex rounded overflow-hidden shadow-lg bg-white w-3/4">
+              <div className="m-4">
+                <h1 className="text-4xl">
+                  {
+                    params.language === "japanese" ? trivia.japanese :
+                    params.language === "english" ? trivia.english:
+                    params.language === "chinese1" ? trivia.chinese1:
+                    params.language === "chinese2" ? trivia.chinese2:
+                    params.language === "korean" ? trivia.korean:
+                    params.language === "mongolian" ? trivia.mongolian:
+                    trivia.japanese
+                  }
+                </h1>
+                
+                <div className="m-2">
+                  <h2 className="text-2xl">Category</h2>
+                  <p>{trivia.category}</p>
+                </div>
+                
+                <div className="m-2">
+                  <h2 className="text-2xl">Map</h2>
+                  {
+                    trivia.address1 ?
+                    <iframe src={trivia.address1} width={400} height={300}></iframe> :
+                    <div></div>
+                  }  
+                </div>
+
+                <div className="m-2">
+                  {
+                    trivia.address2 ?
+                    <iframe src={trivia.address1} width={400} height={300}></iframe> :
+                    <div></div>
+                  }  
+                </div>
+
+                <div className="m-2">
+                  <h2 className="text-2xl">Reference</h2>
+                  {
+                    trivia.url1 ?
+                    <Link href={trivia.url1}>{trivia.url1}</Link> :
+                    <div></div>
+                  }
+                </div>
+
+                <div className="m-2">
+                  {
+                    trivia.url2 ?
+                    <Link href={trivia.url2}>{trivia.url2}</Link> :
+                    <div></div>
+                  }
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
